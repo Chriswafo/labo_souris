799b803fe9102bdd5ce85d2e9c48cfb303866231
Chriswafo <chriswafo16@gmail.com>
2026-02-24 22:16:16 +0100

    creation de la classe et ajout des attributs en privée

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Souris {
+private:
+    double poids;
+    string couleur;
+    unsigned int age;
+    unsigned int esperance_vie;
+    bool clonee;
+};
+int main (){
+return 0;
+}
