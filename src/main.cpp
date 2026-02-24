e78b25f4189da772a358484be80318d331a5ddd9
Chriswafo <chriswafo16@gmail.com>
2026-02-24 22:35:26 +0100

    feat: ajout du main

--- src/main.cpp
+++ src/main.cpp
@@ -35,5 +35,19 @@ public:
    }
 };
 int main (){
+
+  Souris s1(50.0,"blanche", 2);
+  Souris s2(45.0,"grise",5);
+  Souris s3(s2);
+  // ... un tableau peut-être...
+  s1.afficher();
+  s2.afficher();
+  s3.afficher();
+  s1.evolue();
+  s2.evolue();
+  s3.evolue();
+  s1.afficher();
+  s2.afficher();
+  s3.afficher();
 return 0;
 }
