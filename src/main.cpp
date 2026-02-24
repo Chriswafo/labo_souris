9574ddd1b3d961d709cbb15b6113a70eb695f596
Chriswafo <chriswafo16@gmail.com>
2026-02-24 22:23:53 +0100

    feat: ajout du constructeur de copie des souris

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,9 @@ public:
     Souris(double p,string c, unsigned int a=0,unsigned int e=36):
         poids(p),couleur(c),age(a),esperance_vie(e),clonee(false)
         {cout<<"Une nouvelle souris"<<endl;}
+    Souris(Souris const& autre):
+        poids(autre.poids),couleur(autre.couleur),age(autre.age),esperance_vie(autre.esperance_vie*4/5),clonee(true)
+        {cout<<"clonage d'une souris"<<endl;}
 };
 int main (){
 return 0;
