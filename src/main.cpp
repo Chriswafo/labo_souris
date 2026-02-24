7d1e90d27980f28a64cf4d87870148675eb25d07
Chriswafo <chriswafo16@gmail.com>
2026-02-24 22:21:21 +0100

    feat: ajout du constructeur par defaut de la classe

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,10 @@ private:
     unsigned int age;
     unsigned int esperance_vie;
     bool clonee;
+public:
+    Souris(double p,string c, unsigned int a=0,unsigned int e=36):
+        poids(p),couleur(c),age(a),esperance_vie(e),clonee(false)
+        {cout<<"Une nouvelle souris"<<endl;}
 };
 int main (){
 return 0;
